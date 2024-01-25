7fa471bfb961f465709726ff6a15d9f52617f84c
TyrZeg <robetwinkie@gmail.com>
2024-01-25 16:21:38 -0500

    GUI Not Working

--- Connect4/src/GUI.java
+++ Connect4/src/GUI.java
@@ -1,25 +1,37 @@
 
-import javax.swing.*;
 import java.awt.*;
 
-
+import javax.swing.*;
+ 
 class GUI extends JFrame{
+ 
 
+    GUI() {
+        JButton b1;
+        JLabel l1;
 
-    GUI(){
+        setSize(1920, 1080);
 
-        JPanel content = (JPanel) getContentPane();
-        ImageIcon backgroundImage = new ImageIcon(getClass().getResource("Java UI.jpg"));
-        JLabel label = new JLabel(backgroundImage);
-        content.add(label, BorderLayout.CENTER);
+        setLayout(new BorderLayout());
 
-        setContentPane(content);
+        JLabel background = new JLabel(new ImageIcon(getClass().getResource("Java UI.jpg")));
+        background.setLayout(new GridBagLayout());
 
-        pack();
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
 
-        setLocationRelativeTo(null);
+        l1 = new JLabel("Here is a button");
+        b1 = new JButton("I am a button");
+
+        background.add(l1, gbc);
+
+        gbc.gridy = 1;
+        background.add(b1, gbc);
+
+        add(background);
+
         setVisible(true);
-
     }
 
 }
